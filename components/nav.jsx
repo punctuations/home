8e1c336bca45566c91d077126f0ba79414cd5360
punctuations <matt@dont-ping.me>
2021-01-23 20:07:08 -0800

    select-none on nav

--- components/nav.jsx
+++ components/nav.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Nav() {
 	return (
-		<div className="absolute 2xl:left-4/6 xl:left-4/6 lg:left-4/6 md:left-4/6 sm:left-3/6 left-3/6 top-2/12">
+		<div className="absolute 2xl:left-4/6 xl:left-4/6 lg:left-4/6 md:left-4/6 sm:left-3/6 left-3/6 top-2/12 select-none">
 			<p className="flex flex-row">
 				<motion.span className="m-1.5">
 					<Link href="/">
