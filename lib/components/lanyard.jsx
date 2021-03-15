a535315987ac6e2c0aa47caf0c54af771c758ada
punctuations <matt@dont-ping.me>
2021-03-15 15:01:21 -0700

    fix + Fade in / out on lanyard

--- lib/components/lanyard.jsx
+++ lib/components/lanyard.jsx
@@ -1,10 +1,8 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import dynamic from "next/dynamic";
 import { useLanyard } from "use-lanyard";
 
 const Skeleton = dynamic(() => import("react-skeleton-loader"), { ssr: false });
-
-const list = [];
 
 export default function lanyard() {
 	const snowflake = "291050399509774340";
@@ -14,124 +12,128 @@ export default function lanyard() {
 
 	return (
 		<>
-			{lanyard ? (
-				<motion.div
-					className="flex items-center select-none"
-					initial={{ y: 20, opacity: 0 }}
-					animate={{ y: 0, opacity: 1 }}
-					exit={{ y: 20, opacity: 0 }}
-				>
-					{lanyard.listening_to_spotify ? (
-						<div className="flex flex-row items-center justify-center space-x-2">
-							<a
-								className="flex flex-row items-center"
-								href={`https://open.spotify.com/search/${lanyard.spotify.song}%20-%20${lanyard.spotify.artist}`}
-								target="_blank"
-							>
-								<figure className="flex flex-row items-end relative">
-									<img
-										src={lanyard.spotify.album_art_url}
-										width={50}
-										height={50}
-										className="rounded-lg"
-									/>
+			<AnimatePresence>
+				{lanyard ? (
+					<motion.div
+						className="flex items-center select-none"
+						initial={{ y: 20, opacity: 0 }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ y: 20, opacity: 0 }}
+					>
+						{lanyard.listening_to_spotify ? (
+							<div className="flex flex-row items-center justify-center space-x-2">
+								<a
+									className="flex flex-row items-center"
+									href={`https://open.spotify.com/search/${lanyard.spotify.song}%20-%20${lanyard.spotify.artist}`}
+									target="_blank"
+								>
+									<figure className="flex flex-row items-end relative">
+										<img
+											src={lanyard.spotify.album_art_url}
+											width={50}
+											height={50}
+											className="rounded-lg"
+										/>
 
-									<figcaption>
-										<svg
-											height={20}
-											width={20}
-											fill="currentColor"
-											viewBox="0 0 496 512"
-											className="absolute -bottom-1.5 -right-1.5 text-green-500 transition-colors duration-300 ease-in-out bg-white dark:bg-black rounded-xl p-0.5"
-										>
-											<path d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z" />
-										</svg>
-									</figcaption>
-								</figure>
-							</a>
-							<div className="flex flex-col text-xs text-black dark:text-white">
-								<p className="font-bold">{lanyard.spotify.song}</p>
-								<p className="text-gray-400 dark:text-gray-600">
-									by {lanyard.spotify.artist}
-								</p>
+										<figcaption>
+											<svg
+												height={20}
+												width={20}
+												fill="currentColor"
+												viewBox="0 0 496 512"
+												className="absolute -bottom-1.5 -right-1.5 text-green-500 transition-colors duration-300 ease-in-out bg-white dark:bg-black rounded-xl p-0.5"
+											>
+												<path d="M248 8C111.1 8 0 119.1 0 256s111.1 248 248 248 248-111.1 248-248S384.9 8 248 8zm100.7 364.9c-4.2 0-6.8-1.3-10.7-3.6-62.4-37.6-135-39.2-206.7-24.5-3.9 1-9 2.6-11.9 2.6-9.7 0-15.8-7.7-15.8-15.8 0-10.3 6.1-15.2 13.6-16.8 81.9-18.1 165.6-16.5 237 26.2 6.1 3.9 9.7 7.4 9.7 16.5s-7.1 15.4-15.2 15.4zm26.9-65.6c-5.2 0-8.7-2.3-12.3-4.2-62.5-37-155.7-51.9-238.6-29.4-4.8 1.3-7.4 2.6-11.9 2.6-10.7 0-19.4-8.7-19.4-19.4s5.2-17.8 15.5-20.7c27.8-7.8 56.2-13.6 97.8-13.6 64.9 0 127.6 16.1 177 45.5 8.1 4.8 11.3 11 11.3 19.7-.1 10.8-8.5 19.5-19.4 19.5zm31-76.2c-5.2 0-8.4-1.3-12.9-3.9-71.2-42.5-198.5-52.7-280.9-29.7-3.6 1-8.1 2.6-12.9 2.6-13.2 0-23.3-10.3-23.3-23.6 0-13.6 8.4-21.3 17.4-23.9 35.2-10.3 74.6-15.2 117.5-15.2 73 0 149.5 15.2 205.4 47.8 7.8 4.5 12.9 10.7 12.9 22.6 0 13.6-11 23.3-23.2 23.3z" />
+											</svg>
+										</figcaption>
+									</figure>
+								</a>
+								<div className="flex flex-col text-xs text-black dark:text-white">
+									<p className="font-bold">{lanyard.spotify.song}</p>
+									<p className="text-gray-400 dark:text-gray-600">
+										by {lanyard.spotify.artist}
+									</p>
+								</div>
 							</div>
-						</div>
-					) : (
-						<>
-							{activity ? (
-								<>
-									{activity.assets && (
-										<div className="flex flex-row items-center justify-center space-x-2">
-											<figure className="flex flex-row items-end relative">
-												<img
-													src={`https://cdn.discordapp.com/app-assets/${BigInt(
-														activity.application_id
-													).toString()}/${activity.assets.large_image}.png`}
-													alt={
-														activity.assets.large_text
-															? activity.assets.large_text
-															: null
-													}
-													width={50}
-													height={50}
-													className="rounded-lg"
-												/>
+						) : (
+							<>
+								{activity ? (
+									<>
+										{activity.assets && (
+											<div className="flex flex-row items-center justify-center space-x-2">
+												<figure className="flex flex-row items-end relative">
+													<img
+														src={`https://cdn.discordapp.com/app-assets/${BigInt(
+															activity.application_id
+														).toString()}/${activity.assets.large_image}.png`}
+														alt={
+															activity.assets.large_text
+																? activity.assets.large_text
+																: null
+														}
+														width={50}
+														height={50}
+														className="rounded-lg pointer-events-none"
+													/>
 
-												{activity.assets.application_id ? (
-													<figcaption>
-														<img
-															src={`https://cdn.discordapp.com/app-assets/${BigInt(
-																activity.application_id
-															).toString()}/${activity.assets.small_image}.png`}
-															alt={
-																activity.assets.small_text
-																	? activity.assets.small_text
-																	: null
-															}
-															width={20}
-															height={20}
-															className="rounded-full absolute -bottom-1.5 -right-1.5 text-green-500 transition-colors duration-300 ease-in-out bg-white dark:bg-black p-0.5"
-														/>
-													</figcaption>
-												) : null}
-											</figure>
+													{activity.assets.application_id ? (
+														<figcaption>
+															<img
+																src={`https://cdn.discordapp.com/app-assets/${BigInt(
+																	activity.application_id
+																).toString()}/${
+																	activity.assets.small_image
+																}.png`}
+																alt={
+																	activity.assets.small_text
+																		? activity.assets.small_text
+																		: null
+																}
+																width={20}
+																height={20}
+																className="rounded-full absolute -bottom-1.5 -right-1.5 text-green-500 transition-colors duration-300 ease-in-out bg-white dark:bg-black p-0.5"
+															/>
+														</figcaption>
+													) : null}
+												</figure>
 
-											<div className="flex flex-col text-xs text-black dark:text-white">
-												<p className="font-bold">
-													{activity.assets.small_text ? (
-														activity.assets.small_text
-													) : (
-														<>
-															{activity.assets.large_text ? (
-																activity.assets.large_text
-															) : (
-																<>{activity.name ? activity.name : null}</>
-															)}
-														</>
-													)}
-												</p>
-												<p>
-													{activity.state ? (
-														activity.state
-													) : (
-														<>{activity.details ? activity.details : null}</>
-													)}
-												</p>
+												<div className="flex flex-col text-xs text-black dark:text-white">
+													<p className="font-bold">
+														{activity.assets.small_text ? (
+															activity.assets.small_text
+														) : (
+															<>
+																{activity.assets.large_text ? (
+																	activity.assets.large_text
+																) : (
+																	<>{activity.name ? activity.name : null}</>
+																)}
+															</>
+														)}
+													</p>
+													<p>
+														{activity.state ? (
+															activity.state
+														) : (
+															<>{activity.details ? activity.details : null}</>
+														)}
+													</p>
+												</div>
 											</div>
-										</div>
-									)}
-								</>
-							) : null}
-						</>
-					)}
-				</motion.div>
-			) : (
-				<div className="flex space-x-3 items-end">
-					<Skeleton width="50px" height="50px" widthRandomness={0} />
-					<Skeleton count={2} />
-					<Skeleton />
-				</div>
-			)}
+										)}
+									</>
+								) : null}
+							</>
+						)}
+					</motion.div>
+				) : (
+					<div className="flex space-x-3 items-end">
+						<Skeleton width="50px" height="50px" widthRandomness={0} />
+						<Skeleton count={2} />
+						<Skeleton />
+					</div>
+				)}
+			</AnimatePresence>
 		</>
 	);
 }
