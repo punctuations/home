4ebe6ddc2df403f37b5be693451a0d81058937f6
Matt <matt@dont-ping.me>
2021-04-28 23:40:22 -0700

    js

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,9 @@
-import Document, {
-  DocumentContext,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
+    return { ...initialProps };
   }
 
   render() {
