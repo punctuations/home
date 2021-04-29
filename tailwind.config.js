00fc686900a11cd86b252acd5a4193a30048fa5a
Matt <matt@dont-ping.me>
2021-04-29 00:01:55 -0700

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
   variants: {
     extend: {
       borderWidth: ["dark"],
     },
   },
-  plugins: [],
 };
