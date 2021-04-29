2dff604887c70247f4c2ffd53141a7efff4c9ec1
Matt <matt@dont-ping.me>
2021-04-28 23:55:48 -0700

    final attempt

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body className="bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
