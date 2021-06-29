d96ddacd2b132248f646176c0277f2f49e0f0bf0
Matt <matt@dont-ping.me>
2021-06-29 12:43:34 -0700

    feat: background image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,16 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
-        <body className="2xl:overflow-hidden xl:overflow-hidden lg:overflow-hidden overflow-auto bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out">
+        <body
+          className="2xl:overflow-hidden xl:overflow-hidden lg:overflow-hidden overflow-auto bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out"
+          style={{
+            backgroundImage:
+              "radial-gradient(#e3e3e3 1px,transparent 0), radial-gradient(#e3e3e3 1px,transparent 0)",
+            backgroundPosition: "0 0,25px 25px",
+            backgroundAttachment: "fixed",
+            backgroundSize: "50px 50px",
+          }}
+        >
           <Main />
           <NextScript />
         </body>
