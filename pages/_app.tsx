4aa633c3cc7657c6c68144b94dbc99d2559bfbf0
Matt <matt@dont-ping.me>
2021-06-29 14:02:53 -0700

    fix: background dark/light

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "tailwindcss/tailwind.css";
+import "../styles/tailwind.css";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,25 +6,37 @@ import Document, {
   NextScript,
 } from "next/document";
 
+import { useState } from "react";
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return initialProps;
   }
+
   render() {
+    let theme = 0;
+
+    if (process.browser) {
+      setInterval(() => {
+        if (!localStorage.getItem("theme")) {
+          theme = 0;
+        } else {
+          theme = parseInt(localStorage.getItem("theme") as string);
+          parseInt(localStorage.getItem("theme") as string)
+            ? document.querySelector("html")?.classList.remove("light")
+            : document.querySelector("html")?.classList.remove("dark");
+          parseInt(localStorage.getItem("theme") as string)
+            ? document.querySelector("html")?.classList.add("dark")
+            : document.querySelector("html")?.classList.add("light");
+        }
+      }, 200);
+    }
+
     return (
       <Html lang="en">
         <Head />
-        <body
-          className="2xl:overflow-hidden xl:overflow-hidden lg:overflow-hidden overflow-auto bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out"
-          style={{
-            backgroundImage:
-              "radial-gradient(#e3e3e3 1px,transparent 0), radial-gradient(#e3e3e3 1px,transparent 0)",
-            backgroundPosition: "0 0,25px 25px",
-            backgroundAttachment: "fixed",
-            backgroundSize: "50px 50px",
-          }}
-        >
+        <body className="dots dark:dots-dark 2xl:overflow-hidden xl:overflow-hidden lg:overflow-hidden overflow-auto bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out">
           <Main />
           <NextScript />
         </body>
