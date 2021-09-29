8f74ba34eb11b94ea0d3603828fafd0cc04a2676
matt <mattsnaccc@gmail.com>
2021-09-29 11:19:38 -0700

    feat: update teaser :)

--- pages/hi.tsx
+++ pages/hi.tsx
@@ -36,7 +36,7 @@ export default function Hi() {
 
       <main className="absolute w-full h-full flex flex-col items-center justify-center">
         <Message.Provider>
-          <Message text={"Oh, I'm surprised your here."} />
+          <Message text={"2021-10-1"} />
           <Message text={"👀💬 soon™️"} />
         </Message.Provider>
       </main>
