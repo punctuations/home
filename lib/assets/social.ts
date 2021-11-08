ce9798a9afd4360a85696ccf9e89b05dc720df55
matt <mattsnaccc@gmail.com>
2021-11-08 10:48:25 -0800

    feat: allow syncing of projects across multiple projects

---
+++ lib/assets/social.ts
@@ -0,0 +1,5 @@
+export const Socials = {
+    twitter: {name: 'matt', discriminator: null, at: '@atmattt', href: 'https://twitter.com/atmattt'},
+    github: {name: 'Matt', discriminator: null, at: 'punctuations', href: 'https://github.com/punctuations'},
+    discord: {name: 'mattt', discriminator: '0596', at: 'mattt#0596', href: 'https://discord.com/users/291050399509774340'}
+}
