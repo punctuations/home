d4d9407fdac4ede538a06cc676ea998e6b911f50
matt <matt@dont-ping.me>
2023-06-24 13:42:52 -0700

    google

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,15 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script async src="https://cdn.splitbee.io/sb.js" />
+          
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q290BD7D61"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Q290BD7D61');
+          </script>
         </Head>
         <body className="light-select dark:dark-select 2xl:overflow-hidden xl:overflow-hidden overflow-auto bg-white dark:bg-black bg dark:bg-dark transition-colors duration-300 ease-in-out">
           <Main />
