db8ba84e2a4aa519c9199d598338e2feabc2b57f
matt <matt@dont-ping.me>
2023-11-19 00:35:03 -0800

    fix: email

--- post/out/social.js
+++ post/out/social.js
@@ -30,28 +30,28 @@ exports.Socials = void 0;
  * }
  */
 exports.Socials = {
-    twitter: {
-        name: "matt",
-        discriminator: null,
-        at: "@atmattt",
-        href: "https://twitter.com/atmattt"
-    },
-    github: {
-        name: "Matt",
-        discriminator: null,
-        at: "punctuations",
-        href: "https://github.com/punctuations"
-    },
-    discord: {
-        name: "mattt",
-        discriminator: "0596",
-        at: "mattt#0596",
-        href: "https://discord.com/users/291050399509774340"
-    },
-    email: {
-        name: "matt",
-        discriminator: null,
-        at: "matt@dont-ping.me",
-        href: "mailto:matt@dont-ping.me"
-    }
+	twitter: {
+		name: "matt",
+		discriminator: null,
+		at: "@atmattt",
+		href: "https://twitter.com/atmattt",
+	},
+	github: {
+		name: "Matt",
+		discriminator: null,
+		at: "punctuations",
+		href: "https://github.com/punctuations",
+	},
+	discord: {
+		name: "mattt",
+		discriminator: "0596",
+		at: "mattt#0596",
+		href: "https://discord.com/users/291050399509774340",
+	},
+	email: {
+		name: "matt",
+		discriminator: null,
+		at: "hey@mattt.space",
+		href: "mailto:hey@mattt.space",
+	},
 };
