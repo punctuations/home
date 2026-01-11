82c2952c433f6d7c54a09c482e6ef98c818be9be
Matt <hey@mattt.space>
2026-01-10 23:06:39 -0800

    fix: twt

--- next.config.ts
+++ next.config.ts
@@ -22,12 +22,12 @@ const nextConfig: NextConfig = {
       },
       {
         source: "/twitter",
-        destination: "https://twitter.com/1096bit",
+        destination: "https://twitter.com/0xA5A5",
         permanent: true,
       },
       {
         source: "/x",
-        destination: "https://x.com/1096bit",
+        destination: "https://x.com/0xA5A5",
         permanent: true,
       },
       {
