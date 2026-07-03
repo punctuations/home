2b5aa4ded549a901c36218545752381b32edd885
Matt <hey@mattt.space>
2026-07-03 00:38:19 -0700

    email

--- components/links/email.tsx
+++ components/links/email.tsx
@@ -1,62 +1,62 @@
 "use client";
 
 export const Email = () => {
-	return (
-		<div
-			style={{
-				width: "240px",
-				borderRadius: "8px",
-				boxShadow: "0 4px 6px rgba(0,0,0,0.07), 0 12px 32px rgba(0,0,0,0.12)",
-				fontFamily: "monospace",
-				overflow: "hidden",
-			}}
-			className="reciept"
-		>
-			<a
-				href="mailto:hey@mattt.space"
-				target="_blank"
-				rel="noopener noreferrer"
-				style={{
-					padding: "12px",
-					display: "flex",
-					alignItems: "center",
-					gap: "10px",
-				}}
-			>
-				<svg
-					width="16"
-					height="16"
-					viewBox="0 0 16 16"
-					fill="none"
-					xmlns="http://www.w3.org/2000/svg"
-				>
-					<path
-						d="M16 14H0V2H16V14ZM12 6H10V8H6V6H4V8H6V10H10V8H12V6H14V4H12V6ZM2 6H4V4H2V6Z"
-						fill="black"
-					/>
-				</svg>
+  return (
+    <div
+      style={{
+        width: "240px",
+        borderRadius: "8px",
+        boxShadow: "0 4px 6px rgba(0,0,0,0.07), 0 12px 32px rgba(0,0,0,0.12)",
+        fontFamily: "monospace",
+        overflow: "hidden",
+      }}
+      className="reciept"
+    >
+      <div
+        // href="mailto:hey@mattt.space"
+        // target="_blank"
+        // rel="noopener noreferrer"
+        style={{
+          padding: "12px",
+          display: "flex",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <svg
+          width="16"
+          height="16"
+          viewBox="0 0 16 16"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M16 14H0V2H16V14ZM12 6H10V8H6V6H4V8H6V10H10V8H12V6H14V4H12V6ZM2 6H4V4H2V6Z"
+            fill="black"
+          />
+        </svg>
 
-				<div
-					style={{
-						display: "flex",
-						flexDirection: "column",
-						minWidth: 0,
-					}}
-				>
-					<span
-						style={{
-							fontSize: "11px",
-							fontWeight: "bold",
-							color: "#0f172a",
-							overflow: "hidden",
-							textOverflow: "ellipsis",
-							whiteSpace: "nowrap",
-						}}
-					>
-						hey@mattt.space
-					</span>
-				</div>
-			</a>
-		</div>
-	);
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            minWidth: 0,
+          }}
+        >
+          <span
+            style={{
+              fontSize: "11px",
+              fontWeight: "bold",
+              color: "#0f172a",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            hey[at]mattt[dot]space
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 };
